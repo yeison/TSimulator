ae3cee7b562b4bf00b8a46a6aee032d1083c0ec5
Yeison Rodriguez <jaynano@gmail.com>
2009-09-16 18:35:33 -0400

    Started to modify the Rdoc files to contain help documentation.
    This is the first commit, and the updates made to the files include documentation
    for the applyDegradation function.  The next update will include some changes to
    the applyDegradation function itself.

--- R/applyDegradation.R
+++ R/applyDegradation.R
@@ -1,6 +1,6 @@
 applyDegradation <-
-function(bm){
-	diag(bm) = rnorm(length(bm[1,]), -0.0006, 0.0002)
-	return(bm)
+function(interactionMatrix){
+	im <- interactionMatrix
+	diag(im) = rnorm(length(im[1,]), -0.0006, 0.0002)
+	return(im)
 }
-
