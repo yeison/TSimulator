0a5b30c207fac08b2807ff89826bce1adf3401cd
Yeison Rodriguez <yeirodr@gmail.com>
2010-01-25 12:57:59 -0500

    Rdoc might have changed version.  All doc files modified. README added.
    There was an error installing the package into R.  All of the documentation title sections had to be modified.  The title section was not allowed to be empty.  Added a README file.  It contains the same information as the DESCRIPTION file.

--- R/extractGNW.R
+++ R/extractGNW.R
@@ -4,9 +4,8 @@ function(synth, npoints) {
 	cols = dim(synth)[2]
 	dt = rows/npoints	
 	extracted = matrix(0, nrow=cols, ncol=npoints)
-	extracted[,1] = as.matrix(synth[1,])
+	extracted[ ,1] = as.matrix(synth[1, ])
 	for (i in 2:npoints)
 		extracted[,i] = as.matrix(synth[i*dt, ])
 	return(extracted)
 }
-
