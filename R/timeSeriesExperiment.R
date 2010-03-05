ce489a0d2be36b423d37a6d7d79d88c847be9465
Yeison Rodriguez <yeirodr@gmail.com>
2010-03-05 16:57:50 -0500

    Several important modifications: Efficiency, featured measurements.
    Efficiency has been greatly improved by using R's builtin transposition function instead of my own.
    
    Calculation of spread out time points has been included, and is output by default.

--- R/timeSeriesExperiment.R
+++ R/timeSeriesExperiment.R
@@ -8,40 +8,60 @@ loadGNWData <- function(inFolderName, outFolderName="TS_Output", loadDir=path.ex
 	stoch = list()
 	for(series in seq(seriesCount)){
 		seriesPath = paste(loadPath, seriesType[1], series, sep="/")
-		stoch = append(stoch, toList(timeSeriesExperiment(seriesPath), stoch, series))
+		stoch = toList(stoch, seriesPath, series)
 	}
 	
 	#seriesType two: Deterministic
 	deter = list()
 	for(series in seq(seriesCount)){
 		seriesPath = paste(loadPath, seriesType[2], series, sep="/")
-		deter = toList(timeSeriesExperiment(seriesPath), deter, series)
+		deter = toList(deter, seriesPath, series)
 	}
+	
+	sSpread = list()
+	for(series in seq(seriesCount)){
+		seriesPath = paste(loadPath, seriesType[1], series, sep="/")
+		sSpread = toList(sSpread, seriesPath, series, spread=10)
+	}
+	
+	#seriesType two: Deterministic
+	dSpread = list()
+	for(series in seq(seriesCount)){
+		seriesPath = paste(loadPath, seriesType[2], series, sep="/")
+		dSpread = toList(dSpread, seriesPath, series, spread=10)
+	}
+	
 	
 	#seriesType three: Stochastic and Deterministic, 21 data points.
 	for(series in seq(length(seriesType) - 1)){
-		for(series in seq(seriesCount))
+		sEven = list()
+		for(series in seq(seriesCount)){
 			seriesPath = paste(loadPath, seriesType[3], seriesType[1], series, sep="/")
-			stoch21 = timeSeriesExperiment(seriesPath, 21)
-		for(series in seq(seriesCount))
+			sEven = toList(sEven, seriesPath, series)
+		}
+		dEven = list()
+		for(series in seq(seriesCount)){
 			seriesPath = paste(loadPath, seriesType[3], seriesType[2], series, sep="/")
-			deter21 = timeSeriesExperiment(seriesPath, 21)
+			dEven = toList(dEven, seriesPath, series)
+		}
 	}
 
 	savePath = createSavePath(outFolderName)
-	save(stoch, deter, file=savePath)
+	save(stoch, deter, sSpread, dSpread, sEven, dEven, file=savePath)
 }
 
-timeSeriesExperiment <- function(seriesPath, npoints=1000){
+timeSeriesExperiment <- function(seriesPath, spread=1, npoints=1000){
 	seriesPath = paste(seriesPath, "Ecoli-1_dream4_timeseries.tsv", sep="/")
 	seriesData = readGNW(seriesPath, npoints, plot=FALSE)
 	print("Finished")
-	extracted = extractRegionals(seriesData)
+	extracted = extractRegionals(seriesData, spread=spread)
 	return(extracted)
 }
 
-toList <- function(data, theList, seriesNumber){
-	tempList = list(data)	
+toList <- function(theList, seriesPath, seriesNumber, spread=1){
+	data = timeSeriesExperiment(seriesPath, spread=spread)
+	tempList = list(data)
 	names(tempList) = paste("ts", seriesNumber, sep="")
-	return(tempList)
+	theList = append(theList, tempList)
+	return(theList)
 }
